c3c5958dd59d16f08df39ca306d6cdfab3294d89
Barend Koorzen <koorzenb@gmail.com>
2023-01-10 08:26:06 +0200

    Minor updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 console.log('app running');
 
-var template = <h1>My App</h1>
+var template = <div><h1>My App</h1></div>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);

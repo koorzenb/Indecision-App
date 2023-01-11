41ec63836dba2d39a2c89760935f8e50b39c83db
Barend Koorzen <koorzenb@gmail.com>
2023-01-11 09:27:47 +0200

    JSX Expressions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 console.log('app running');
 
-var template = <div><h1>My App</h1></div>;
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your hands in the life of a numbercruncher'
+}
+
+var template = <div>
+<h1>{app.title}</h1>
+<p>{app.subtitle}</p>
+</div>;
+
+const user = {
+    userName: "Barend",
+    userAge: '34',
+    userLocation: 'Bellville'
+}
+const templateTwo = (
+    <div>
+        <h1>{user.userName}</h1>
+        <p>Age: {user.userAge}</p>
+        <p>Location: {user.userLocation}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);

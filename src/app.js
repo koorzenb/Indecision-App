97abf4236d73a502da49c4da6135365f7faadf22
Barend Koorzen <koorzenb@gmail.com>
2023-01-21 07:17:05 +0200

    Saving and Loading Options Data

--- src/app.js
+++ src/app.js
@@ -10,17 +10,36 @@ class IndecisionApp extends React.Component {
       options: props.options,
     };
   }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (error) {
+      // Do nothing
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      {
+        const json = JSON.stringify(this.state.options);
+        localStorage.setItem('options', json);
+      }
+    }
+  }
 
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
   }
 
   handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => {
-      return {
-        options: prevState.options.filter((option) => option != optionToRemove),
-      };
-    });
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option),
+    }));
   }
 
   handlePick() {
@@ -96,11 +115,14 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && (
+        <p>Please add an options to get started</p>
+      )}
       {props.options.map((option) => (
         <Option
           key={option}
           optionText={option}
-          handleDeleteOption={handleDeleteOption}
+          handleDeleteOption={props.handleDeleteOption}
         />
       ))}
     </div>
@@ -138,6 +160,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState((prevState) => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
   render() {
     return (

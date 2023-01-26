8be96e376939e9cda00ba931139d832ee45dd885
Barend Koorzen <koorzenb@gmail.com>
2023-01-26 08:39:03 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -4,3 +4,13 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/Indecision App';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class NewSyntax {
+  name = 'Jen';
+  getGreeting() {
+    return `My name is ${this.name}`;
+  }
+}
+
+const newSyntax = new NewSyntax();
+newSyntax.getGreeting();

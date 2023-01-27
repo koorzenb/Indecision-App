da2eefbc6585d8dd29f870b63b09ba2b90f820f6
Barend Koorzen <koorzenb@gmail.com>
2023-01-27 05:21:25 +0200

    wip

--- src/components/OptionsModal.js
+++ src/components/OptionsModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-  <Modal isOpen={props.selectedOption} contentLabel="Selected Option">
+  <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
     <h3>Selected Option</h3>
   </Modal>
 );

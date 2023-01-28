f6617bd5d5818a38b0738a231c406d86a0abd6ca
Barend Koorzen <koorzenb@gmail.com>
2023-01-28 12:50:35 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles/styles.scss';
 import IndecisionApp from './components/Indecision App';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
